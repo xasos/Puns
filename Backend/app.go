7a82cb577e5df04202d9f90f9d8cb8f1e59ff58e
Niraj Pant <nirajpant7@gmail.com>
2014-11-26 05:54:21 +0000

    add json

--- Backend/app.go
+++ Backend/app.go
@@ -1,11 +1,23 @@
 package main
 
-import "github.com/codegangsta/martini"
+import (
+	"fmt"
+	"github.com/codegangsta/martini"
+	"encoding/json"
+	"io/ioutil"
+)
 
 func main() {
     m := martini.Classic()
 
-    m.Get("/", func() string {
+    file, e := ioutil.ReadFile("./puns.json")    
+    if e != nil {
+        fmt.Printf("File error: %v\n", e)
+        os.Exit(1)
+    }
+    fmt.Printf("%s\n", string(file))
+
+    m.Get("/api/random", func() string {
         return "test"
     })
 
