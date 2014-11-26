3accb142a1205d20d5b7d15b5cc15042b62bf456
Niraj Pant <nirajpant7@gmail.com>
2014-11-26 20:01:10 +0000

    move random number into api

--- Backend/app.go
+++ Backend/app.go
@@ -1,36 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/codegangsta/martini"
 	"io/ioutil"
 	"math/rand"
-	"encoding/json"
 	"time"
 )
 
 func main() {
-    m := martini.Classic()
-    
-    type Puns struct {
-	Pun string
-    }
-    
-    // Import Puns.json file and serialize into go struct
-    file, e := ioutil.ReadFile("./puns.json")    
-    if e != nil {
-        fmt.Printf("File error: %v\n", e)
-    }
+	m := martini.Classic()
 
-       p := Puns{}
-       json.Unmarshal([]byte(file), &p)
+	type Puns struct {
+		Pun string
+	}
 
-       rand.Seed(time.Now().UnixNano())
-       fmt.Println(rand.Intn(100))
-    
-    m.Get("/api/random", func() string {
-	return "test"
-    })
+	// Import Puns.json file and serialize into go struct
+	file, e := ioutil.ReadFile("./puns.json")
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+	}
 
-    m.Run()
+	p := Puns{}
+	json.Unmarshal([]byte(file), &p)
+
+	m.Get("/api/random", func() string {
+		rand.Seed(time.Now().UnixNano())
+	        fmt.Println(rand.Intn(100))
+		return "test"
+	})
+
+	m.Run()
 }
