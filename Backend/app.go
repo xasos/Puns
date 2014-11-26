54d08cf97a4d9b333962f812475a0694ffca3d9e
Niraj Pant <nirajpant7@gmail.com>
2014-11-26 18:33:16 +0000

    add app.go

--- Backend/app.go
+++ Backend/app.go
@@ -10,6 +10,10 @@ import (
 
 func main() {
     m := martini.Classic()
+    
+    type jsonobject struct {
+    Object ObjectType
+    }
 
     file, e := ioutil.ReadFile("./puns.json")    
     if e != nil {
@@ -20,6 +24,10 @@ func main() {
     var jsontype jsonobject
     json.Unmarshal(file, &jsontype)
     fmt.Printf("Results: %v\n", jsontype)
+
+    s1 := rand.NewSource(42)
+    r1 := rand.New(s1)
+    fmt.Print(r1.Intn(100))
 
     m.Get("/api/random", func() string {
         return "test"
